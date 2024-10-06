b7dd181c2f601501bd65db0cf696583bbfd3adb3
Simeon <simeonthedeveloper@gmail.com>
2024-10-06 13:54:49 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Route, Router } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div className='text-3xl text-red-400 font-bold underline' >Hello World</div>
+  )
+}
+
+export default App
